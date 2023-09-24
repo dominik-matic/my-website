3c9bb78dc951db81b61395a94582459d12866ae6
Dominik Matić <dm51746@fer.hr>
2023-09-24 14:21:55 +0200

    added initial node app code

---
+++ app/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs')
+})
+
+app.get('/mylinks', (req, res) => {
+  res.render('mylinks.ejs')
+})
+
+
+
+app.use(express.static(__dirname + '/public')); 
