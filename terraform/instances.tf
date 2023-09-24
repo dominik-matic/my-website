8ff3bd4cb8d3128728ffeb2897a2602fb1730c72
Dominik Matić <dm51746@fer.hr>
2023-09-24 14:22:53 +0200

    added terraform files

---
+++ terraform/instances.tf
@@ -0,0 +1,37 @@
+
+# Create a key pair
+resource "aws_key_pair" "dominik" {
+  key_name   = "dominik-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPsBWEvoZTh2AWoSoR2uexU2UXjhbBLXavHwJ2OklgYe dominik@Cerberus"
+}
+
+# Create an EC2 instance
+resource "aws_instance" "my_server" {
+  ami           = "ami-0b9094fa2b07038b8"
+  instance_type = "t2.micro"
+
+  private_ip = "10.0.0.10"
+  subnet_id  = aws_subnet.my_subnet.id
+
+  key_name = aws_key_pair.dominik.key_name
+
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+
+
+  tags = {
+    Name = "my_server"
+  }
+}
+
+# Create an Elastic IP
+resource "aws_eip" "my_eip" {
+  domain = "vpc"
+
+  instance                  = aws_instance.my_server.id
+  associate_with_private_ip = "10.0.0.10"
+  depends_on                = [aws_internet_gateway.my_gw]
+
+  tags = {
+    Name = "my_eip"
+  }
+}
---
+++ terraform/network.tf
@@ -0,0 +1,111 @@
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my_vpc"
+  }
+}
+
+# Create a gateway
+resource "aws_internet_gateway" "my_gw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my_gw"
+  }
+}
+
+# Create a subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  depends_on = [aws_internet_gateway.my_gw]
+
+  tags = {
+    Name = "my_subnet"
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "my_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_gw.id
+  }
+
+  tags = {
+    Name = "my_rt"
+  }
+}
+
+# Associate the created route table with the created subnet
+resource "aws_route_table_association" "rt_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_rt.id
+}
+
+# Create a security group
+resource "aws_security_group" "my_sg" {
+  name        = "my security group"
+  description = "my security group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  # Allow SSH
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Allow HTTP
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Allow HTTPS
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  # Allow ICMP
+  ingress {
+    description      = "ICMP"
+    from_port        = -1
+    to_port          = -1
+    protocol         = "icmp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  # Allow all outgoing traffic
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "my_sg"
+  }
+}
