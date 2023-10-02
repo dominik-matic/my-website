adacb16793427d1587f5aabf4a8de37bfbf856bb
Dominik Matić <dm51746@fer.hr>
2023-10-02 20:41:52 +0200

    test

--- app/app.js
+++ app/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const https = require('https')
 const port = 3000
 
 app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public')); 
 
+app.use((req, res, next) => {
+  if(req.protocol === 'http') {
+    return res.redirect(301, `https://${req.headers.host}${req.url}`);
+  }
+  next();
+})
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
@@ -19,6 +27,10 @@ app.get('/mylinks', (req, res) => {
   res.render('mylinks.ejs')
 })
 
-app.listen(port, () => {
+https.createServer({
+  key: fs.readFileSync('/etc/letsencrypt/private.key.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/domain.cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/intermediate.cert.pem')
+}).listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
