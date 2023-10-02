5e76ea8866596f95bb91c9b9c7ff91ed7a88ea88
Dominik Matić <dm51746@fer.hr>
2023-10-02 20:44:00 +0200

    fix, still no ssl

--- app/app.js
+++ app/app.js
@@ -1,19 +1,11 @@
 const express = require('express')
 const app = express()
-const fs = require('fs')
-const https = require('https')
 const port = 3000
 
 app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public')); 
 
-app.use((req, res, next) => {
-  if(req.protocol === 'http') {
-    return res.redirect(301, `https://${req.headers.host}${req.url}`);
-  }
-  next();
-})
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
@@ -27,10 +19,6 @@ app.get('/mylinks', (req, res) => {
   res.render('mylinks.ejs')
 })
 
-https.createServer({
-  key: fs.readFileSync('/etc/letsencrypt/private.key.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/domain.cert.pem'),
-  ca: fs.readFileSync('/etc/letsencrypt/intermediate.cert.pem')
-}).listen(port, () => {
+app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
