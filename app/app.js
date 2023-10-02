ee1c874b4415ae844a28a7eb88aa6c19f8106a89
Dominik Matić <dm51746@fer.hr>
2023-10-02 21:07:56 +0200

    hope it works now

--- app/app.js
+++ app/app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const fs = require('node:fs')
+const https = require('node:https')
+const http = require('node:http')
+const httpsport = 3443
+const httpport = 3080
+
 
 app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public')); 
 
+app.use((req, res, next) => {
+  if(req.protocol === 'http') {
+    return res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+  next();
+})
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
@@ -19,6 +30,16 @@ app.get('/mylinks', (req, res) => {
   res.render('mylinks.ejs')
 })
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+const options =  {
+  key: fs.readFileSync('/etc/letsencrypt/private.key.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/domain.cert.pem'),
+  //ca: fs.readFileSync('/etc/letsencrypt/intermediate.cert.pem')
+}
+
+https.createServer(options, app).listen(httpsport, () => {
+  console.log(`Server listening on port ${httpsport}`)
+})
+
+http.createServer(app).listen(httpport, () => {
+  console.log(`Http server listening on port ${httpport}`)
 })
