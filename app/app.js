e504127f34bdaebd5a1401514a6c61c1d902f872
Dominik Matić <dm51746@fer.hr>
2023-10-26 07:08:58 +0200

    added logging

--- app/app.js
+++ app/app.js
@@ -3,18 +3,39 @@ const app = express()
 const fs = require('node:fs')
 const https = require('node:https')
 const http = require('node:http')
+const winston = require('winston')
 const httpsport = 3443
 const httpport = 3080
+
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(info => `[${info.timestamp}] ${info.message}`)
+  ),
+  transports: [
+    new winston.transports.Console(),
+    // new winston.transports.File({ filename: 'app.log' })
+  ]
+});
 
 
 app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public')); 
 
+// redirect middleware
 app.use((req, res, next) => {
   if(req.protocol === 'http') {
+    logger.info(`Redirected ${req.ip} to HTTPS: ${req.method} ${req.url}`)
     return res.redirect(`https://${req.headers.host}${req.url}`);
   }
+  next();
+})
+
+// logging middleware
+app.use((req, res, next) => {
+  logger.info(`Request from IP ${req.ip}, User Agent: ${req.userAgent}, Method: ${req.method}, URL: ${req.url}`);
+
   next();
 })
 
